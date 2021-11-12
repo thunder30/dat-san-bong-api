54aa6d25a636634ff913dc11e82e77297bb108bd
trisnguyen2511 <75766468+trisnguyen2511@users.noreply.github.com>
2021-11-13 01:31:04 +0700

    CRUD PitchBranch

---
+++ server/middlewares/pitchBranch/validate.middleware.js
@@ -0,0 +1,111 @@
+const mongoose = require('mongoose')
+const ObjectId = mongoose.Types.ObjectId
+const { check, validationResult } = require('express-validator')
+
+
+const validatePost = (req, res, next) => {
+
+    //validate startTime endTime in format hh:mm
+    const startTime = req.body.startTime
+    const endTime = req.body.endTime
+    const regex = /^([0-1]?[0-9]|2[0-3]):([0-5][0-9])$/
+    if (!regex.test(startTime) || !regex.test(endTime))
+    return res.status(400).json({
+        success: false,
+        message: 'Invalid time format!'
+    })
+
+    // Check validate phoneNumber
+    const phoneNumber = req.body.phoneNumber
+    const regexPhoneNumber = /^(0|\+84)(\s|\.)?((3[2-9])|(5[689])|(7[06-9])|(8[1-689])|(9[0-46-9]))(\d)(\s|\.)?(\d{3})(\s|\.)?(\d{3})$/
+    if (!regexPhoneNumber.test(phoneNumber))
+    return res.status(400).json({
+        success: false,
+        message: 'Invalid phone format!'
+    })
+
+    
+
+    validateResult(req, res, next)
+}
+
+const validatePut = (req, res, next) => {
+
+    // check id
+    const id = req.params.id
+    if (!ObjectId.isValid(id))
+    return res.status(400).json({
+        success: false,
+        message: '_id invalid',
+    })
+
+    //validate startTime endTime in format hh:mm
+    const startTime = req.body.startTime
+    const endTime = req.body.endTime
+    const regex = /^([0-1]?[0-9]|2[0-3]):([0-5][0-9])$/
+    if (!regex.test(startTime) || !regex.test(endTime))
+    return res.status(400).json({
+        success: false,
+        message: 'Invalid time format!'
+    })
+
+    // Check validate phoneNumber
+    const phoneNumber = req.body.phoneNumber
+    const regexPhoneNumber = /^(0|\+84)(\s|\.)?((3[2-9])|(5[689])|(7[06-9])|(8[1-689])|(9[0-46-9]))(\d)(\s|\.)?(\d{3})(\s|\.)?(\d{3})$/
+    if (!regexPhoneNumber.test(phoneNumber))
+    return res.status(400).json({
+        success: false,
+        message: 'Invalid phone format!'
+    })
+
+    validateResult(req, res, next)
+}
+
+const validateDelete = (req, res, next) => {
+
+    // check id
+    const id = req.params.id
+    if (!ObjectId.isValid(id))
+    return res.status(400).json({
+        success: false,
+        message: '_id invalid',
+    })
+
+    validateResult(req, res, next)
+}
+
+const validateGetById = (req, res, next) => {
+
+    // check id
+    const id = req.params.id
+    if (!ObjectId.isValid(id))
+    return res.status(400).json({
+        success: false,
+        message: '_id invalid',
+    })
+
+    validateResult(req, res, next)
+}
+
+const validateResult = (req, res, next) => {
+    // Check validate body
+    const errorFormatter = ({ location, msg, param, value, nestedErrors }) => {
+        // Build your resulting errors however you want! String, object, whatever - it works!
+        return `${location}[${param}]: ${msg}`
+    }
+    const result = validationResult(req).formatWith(errorFormatter)
+    if (!result.isEmpty()) {
+        // Response will contain something like
+        // { errors: [ "body[password]: must be at least 10 chars long" ] }
+        return res.status(400).json({
+            success: false,
+            message: 'Validate error!',
+            errors: result.array(),
+        })
+    }
+    next()
+}
+
+
+
+module.exports = { validatePost, validateDelete, validateGetById, validatePut }
---
+++ server/routes/pitchBranch.routes.js
@@ -0,0 +1,200 @@
+const express = require('express')
+const router = express.Router()
+const slugify = require('slugify')
+const PitchBranch = require('../models/pitchBranch')
+const {
+    validatePost,
+    validateGetById,
+    validatePut,
+    validateDelete
+} = require('../middlewares/pitchBranch')
+const { verifyToken } = require('../middlewares/auth')
+
+/**
+ * @POST /api/pitchBranch
+ * @description Create a new pitchBranch
+ */
+router.post('/', verifyToken, validatePost, async (req, res) => {
+
+    try {
+
+        // Verify isAdmin or isOwner
+        const { isAdmin, userId } = req.payload
+        const { owner } = req.body
+        if (!isAdmin && userId !== owner) {
+            return res.status(401).json({
+                success: false,
+                message: `You don't have permission`
+            })
+        }
+
+        const pitchBranch = new PitchBranch({
+            ...req.body,
+        })
+        // save to db
+        let _pitchBranch = await pitchBranch.save()
+        res.status(201).json({
+            success: true,
+            message: 'Create successfully!',
+            _pitchBranch
+        })
+    }
+    catch (error) {
+        res.status(500).json({
+            success: false,
+            message: 'Internal server error!',
+            error,
+        })
+    }
+})
+
+/**
+ * GET /api/pitchBranch/:id
+ * @description Get a pitchBranch by id
+ */
+router.get('/:id', verifyToken, validateGetById, async (req, res) => {
+    try {
+
+        // Verify isAdmin or isOwner
+        const { isAdmin, userId } = req.payload
+        const { owner } = req.body
+        if (!isAdmin && userId !== owner) {
+            return res.status(403).json({
+                success: false,
+                message: `You don't have permission`
+            })
+        }
+
+        const pitchBranch = await PitchBranch.findById(req.params.id).populate('owner')
+        res.status(200).json({
+            success: true,
+            message: 'Get successfully!',
+            pitchBranch
+        })
+    }
+    catch (error) {
+        res.status(500).json({
+            success: false,
+            message: 'Internal server error!',
+            error,
+        })
+    }
+})
+
+/**
+ * @GET /api/pitchBranch/
+ * @description Get all pitchBranch
+ */
+router.get('/', async (req, res) => {
+    try {
+
+        // Verify isAdmin or isOwner
+        const { isAdmin, userId } = req.payload
+        const { owner } = req.body
+        if (!isAdmin && userId !== owner) {
+            return res.status(403).json({
+                success: false,
+                message: `You don't have permission`
+            })
+        }
+
+        const pitchBranch = await PitchBranch.find().populate('owner')
+        res.status(200).json({
+            success: true,
+            message: 'Get successfully!',
+            pitchBranch
+        })
+    }
+    catch (error) {
+        res.status(500).json({
+            success: false,
+            message: 'Internal server error!',
+            error,
+        })
+    }
+})
+
+/**
+ * @Delete /api/pitchBranch/:id
+ * @description Delete a pitchBranch by id
+ */
+router.delete('/:id', verifyToken, validateDelete, async (req, res) => {   
+    try {
+        // Verify isAdmin or isOwner
+        const { isAdmin, userId } = req.payload
+        const { owner } = req.body
+        if (!isAdmin && userId !== owner) {
+            return res.status(403).json({
+                success: false,
+                message: `You don't have permission`
+            })
+        }
+
+        const pitchBranch = await PitchBranch.findById(req.params.id)
+        if (!pitchBranch) {
+            return res.status(404).json({
+                success: false,
+                message: 'PitchBranch not found!',
+            })
+        }
+
+        // delete
+        await pitchBranch.remove()
+        res.status(200).json({
+            success: true,
+            message: 'Delete successfully!',
+        })
+    }
+    catch (error) {
+        res.status(500).json({
+            success: false,
+            message: 'Internal server error!',
+            error,
+        })
+    }
+})
+
+/**
+ * @PUT /api/pitchBranch/:id
+ * @description Update a pitchBranch by id
+ */
+router.put('/:id', verifyToken, validatePut, async (req, res) => {
+    try {
+
+        // Verify isAdmin or isOwner
+        const { isAdmin, userId } = req.payload
+        const { owner } = req.body
+        if (!isAdmin && userId !== owner) {
+            return res.status(403).json({
+                success: false,
+                message: `You don't have permission`
+            })
+        }
+
+        // find pitchBranch
+        const pitchBranch = await PitchBranch.findById(req.params.id)
+        if (!pitchBranch) {
+            return res.status(404).json({
+                success: false,
+                message: 'PitchBranch not found!',
+            })
+        }
+
+        // update
+        await pitchBranch.updateOne(req.body)
+        res.status(200).json({
+            success: true,
+            message: 'Update successfully!',
+        })
+    }
+    catch (error) {
+        res.status(500).json({
+            success: false,
+            message: 'Internal server error!',
+            error,
+        })
+    }
+})
+
+
+module.exports = router
