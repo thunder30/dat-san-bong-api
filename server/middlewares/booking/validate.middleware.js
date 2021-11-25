abb4016d9b000a7fed673f28e10b460fd8c2104f
trisnguyen2511 <75766468+trisnguyen2511@users.noreply.github.com>
2021-11-26 02:36:57 +0700

    pitchBrand.GetDetail.Price fix, add checkout, add confirm

--- server/middlewares/booking/validate.middleware.js
+++ server/middlewares/booking/validate.middleware.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose')
 const ObjectId = mongoose.Types.ObjectId
 const { check, validationResult } = require('express-validator')
+const Status = require('../../models/Status')
+const Booking = require('../../models/Booking')
+const BookingDetail = require('../../models/BookingDetail')
+const Pitch = require('../../models/Pitch')
+const Price = require('../../models/Price')
 
 const validatePost = (req, res, next) => {
 
@@ -12,7 +17,229 @@ const validatePost = (req, res, next) => {
 
 }
 
+const validateCheckout = (req, res, next) => {
+    return[
+        check('startTime', 'startDate is required').not().isEmpty(),
+        check('endTime', 'endDate is required').not().isEmpty(),
+        check('customer', 'customer is required').not().isEmpty(),
+        check('pitch', 'pitch is required').not().isEmpty(),
+        check('pitch').isMongoId().withMessage('pitch is invalid id'),
+        check('customer').isMongoId().withMessage('customer is invalid id'),
+    ]
+}
+
+const validatePostConfirm = (req, res, next) => {
+    return [
+        check('startTime', 'startDate is required').not().isEmpty(),
+        check('endTime', 'endDate is required').not().isEmpty(),
+        check('customer', 'customer is required').not().isEmpty(),
+        check('pitch', 'pitch is required').not().isEmpty(),
+        check('pitch').isMongoId().withMessage('pitch is invalid id'),
+        check('customer').isMongoId().withMessage('customer is invalid id'),
+        check('isPaid', 'isPaid is required').not().isEmpty(),
+        check('status', 'status is required').not().isEmpty(),
+    ]
+}
+
+const validateCheckoutFunction = async (req, res, next) => {
+
+    const {isAdmin, userId} = req.payload
+    const {startTime, endTime, pitch, customer} = req.body
+
+    if(customer !== userId){
+        return res.status(400).json({
+            success: false,
+            message: "Id not yours"
+        })
+    }
+
+    //check startTime is in format dd/mm/yyyy HH:mm
+    const startTimeRegex = /^(0[1-9]|[12][0-9]|3[01])[\/](0[1-9]|1[012])[\/]\d{4} (0[0-9]|1[0-9]|2[0-3]):(0[0-9]|[1-5][0-9])$/
+    const endTimeRegex = /^(0[1-9]|[12][0-9]|3[01])[\/](0[1-9]|1[012])[\/]\d{4} (0[0-9]|1[0-9]|2[0-3]):(0[0-9]|[1-5][0-9])$/
+    if (!startTimeRegex.test(startTime)) {
+        return res.status(400).send({
+            success: false,
+            message: 'startTime is invalid!',
+        })
+    }
+    if (!endTimeRegex.test(endTime)) {
+        return res.status(400).send({
+            success: false,
+            message: 'endTime is invalid!',
+        })
+    }
+
+    //check startTime is before endTime
+    const startTimeArrayAdd = startTime.split(' ')
+    const endTimeArrayAdd = endTime.split(' ')
+    const startDate = startTimeArrayAdd[0].split('/')
+    const endDate = endTimeArrayAdd[0].split('/')
+    const startTimeArray2Add = startTimeArrayAdd[1].split(':')
+    const endTimeArray2Add = endTimeArrayAdd[1].split(':')
+    const startDateTimeAdd = new Date(startDate[2], startDate[1] - 1, startDate[0], startTimeArray2Add[0], startTimeArray2Add[1])
+    const endDateTimeAdd = new Date(endDate[2], endDate[1] - 1, endDate[0], endTimeArray2Add[0], endTimeArray2Add[1])
+    if (startDateTimeAdd > endDateTimeAdd) {
+        return res.status(400).send({
+            success: false,
+            message: 'startTime is after endTime!',
+        })
+    }
+
+    //check startTime is not in the past
+    const currentDate = new Date()
+    if (startDateTimeAdd < currentDate) {
+        return res.status(400).send({
+            success: false,
+            message: 'startTime must be in future!',
+        })
+    }
+
+    const _pitch = await Pitch.findOne({})
+    .where('_id').equals(req.body.pitch)
+    .populate({
+        path: 'pitchType',
+    })
+
+    const _price = await Price.find({})
+    .sort({time: 1})
+    .where('pitchType').equals(_pitch.pitchType)
+    .populate({
+        path: 'time'
+    })
+
+    //make flat startTime in _price
+    const alStartTime = _price.map(item => {
+        return item.time.startTime
+    })
+    //make flat endTime in _price
+    const alEndTime = _price.map(item => {
+        return item.time.endTime
+    })
+
+    //check startTime is in _price
+    // console.log(alStartTime)
+    // console.log(alEndTime)
+    // console.log(startTimeArray2Add)
+    // console.log(endTimeArray2Add)
+    if (!alStartTime.includes(startTimeArrayAdd[1]) || !alStartTime.includes(endTimeArrayAdd[1]) && !alEndTime.includes(startTimeArrayAdd[1]) || !alEndTime.includes(endTimeArrayAdd[1])) {
+        return res.status(400).send({
+            success: false,
+            message: 'this time don\'t have price!',
+        })
+    }
+
+    // console.log(alStartTime)
+    // console.log(alEndTime)
+
+    // check time not conflict with other bookingDetail
+    const bookingDetails = await BookingDetail.find({})
+    .where('pitch').equals(pitch)
+    .select('startTime endTime')
+    for (let i = 0; i < bookingDetails.length; i++) {
+        const startTimeArray = bookingDetails[i].startTime.split(' ')
+        const endTimeArray = bookingDetails[i].endTime.split(' ')
+        const startDate = startTimeArray[0].split('/')
+        const endDate = endTimeArray[0].split('/')
+        const startTimeArray2 = startTimeArray[1].split(':')
+        const endTimeArray2 = endTimeArray[1].split(':')
+        const startDateTime = new Date(startDate[2], startDate[1] - 1, startDate[0], startTimeArray2[0], startTimeArray2[1])
+        const endDateTime = new Date(endDate[2], endDate[1] - 1, endDate[0], endTimeArray2[0], endTimeArray2[1])
+
+        if (startDateTime.toString() === startDateTimeAdd.toString() || startDateTime.toString() == endDateTimeAdd.toString()) {
+            return res.status(400).send({
+                success: false,
+                code: '0000',
+                message: 'Booked already',
+            })
+        }
+
+        if (startDateTime < startDateTimeAdd && endDateTime > startDateTimeAdd) {
+
+            return res.status(400).send({
+                success: false,
+                code: '0101',
+                message: 'Booked already',
+            })
+        }
+        if (startDateTime < endDateTimeAdd && endDateTime > endDateTimeAdd) {
+            return res.status(400).send({
+                success: false,
+                code: '1010',
+                message: 'Booked already',
+            })
+        }
+        if (startDateTime > startDateTimeAdd && endDateTime < endDateTimeAdd) {
+            return res.status(400).send({
+                success: false,
+                code: '1001',
+                message: 'Booked already',
+            })
+        }
+        if (startDateTime < startDateTimeAdd && endDateTime > endDateTimeAdd) {
+            return res.status(400).send({
+                success: false,
+                code: '0110',
+                message: 'Booked already',
+            })
+        }
+
+    }
+
+    // create array Time of bookingDetail request
+    const startTimeArrayAdd2 = startTimeArrayAdd[1].split(':')
+    const startTimeArrayAdd3 = endTimeArrayAdd[1].split(':')
+    let le = 30
+    if(startTimeArrayAdd2[1] == '00')
+    le = 00
+    let arrTime = []
+    for (let i = startTimeArrayAdd2[0].toString(); i <= startTimeArrayAdd3[0]; i++) {
+        if(le == 00){
+            if(i<10){
+                if(i == startTimeArrayAdd2[0])
+                arrTime.push(i + ':' + '00')
+                arrTime.push('0' + i + ':' + '00')
+            }
+            else
+            arrTime.push(i + ':' + '00')
+            le = 30
+        }else{
+            if(i<10){
+                if(i == startTimeArrayAdd2[0])
+                arrTime.push(i + ':' + '30')
+                else
+                arrTime.push('0' + i + ':'+ '30')
+            }
+            else
+            arrTime.push(i + ':' + '30')
+            le = 00
+        }
+        if(i == startTimeArrayAdd3[0] && startTimeArrayAdd3[1] == '30'){
+            if(i<10)
+            arrTime.push('0' + i + ':'+ '30')
+            else
+            arrTime.push(i + ':' + '30')
+        }
+            
+    }
+
+    //compare time for money
+    let sumPrice = 0
+    for(let i = 0; i < _price.length; i++){
+        for(let j = 0; j < arrTime.length; j+=1){
+            if(arrTime[j] == _price[i].time.startTime && arrTime[j+1] == _price[i].time.endTime){
+                sumPrice += _price[i].price
+            }
+        }
+    }
+
+    req.body.price = sumPrice
+    
+    next()
+}
+
+
 const validatePostFunction = (req, res, next) => {
+    
     //check startDate is dd/mm/yyyy
     const startDate = req.body.startDate
     const endDate = req.body.endDate
@@ -48,8 +275,214 @@ const validatePostFunction = (req, res, next) => {
         })
     }
 
+    
+
     next()
     
+
+}
+
+const validatePostConfirmFunction = async (req, res, next) => {
+
+    const {isAdmin, userId} = req.payload
+    const {startTime, endTime, pitch, customer} = req.body
+
+    if(customer !== userId){
+        return res.status(400).json({
+            success: false,
+            message: "Id not yours"
+        })
+    }
+
+    //check startTime is in format dd/mm/yyyy HH:mm
+    const startTimeRegex = /^(0[1-9]|[12][0-9]|3[01])[\/](0[1-9]|1[012])[\/]\d{4} (0[0-9]|1[0-9]|2[0-3]):(0[0-9]|[1-5][0-9])$/
+    const endTimeRegex = /^(0[1-9]|[12][0-9]|3[01])[\/](0[1-9]|1[012])[\/]\d{4} (0[0-9]|1[0-9]|2[0-3]):(0[0-9]|[1-5][0-9])$/
+    if (!startTimeRegex.test(startTime)) {
+        return res.status(400).send({
+            success: false,
+            message: 'startTime is invalid!',
+        })
+    }
+    if (!endTimeRegex.test(endTime)) {
+        return res.status(400).send({
+            success: false,
+            message: 'endTime is invalid!',
+        })
+    }
+
+    //check startTime is before endTime
+    const startTimeArrayAdd = startTime.split(' ')
+    const endTimeArrayAdd = endTime.split(' ')
+    const startDate = startTimeArrayAdd[0].split('/')
+    const endDate = endTimeArrayAdd[0].split('/')
+    const startTimeArray2Add = startTimeArrayAdd[1].split(':')
+    const endTimeArray2Add = endTimeArrayAdd[1].split(':')
+    const startDateTimeAdd = new Date(startDate[2], startDate[1] - 1, startDate[0], startTimeArray2Add[0], startTimeArray2Add[1])
+    const endDateTimeAdd = new Date(endDate[2], endDate[1] - 1, endDate[0], endTimeArray2Add[0], endTimeArray2Add[1])
+    if (startDateTimeAdd > endDateTimeAdd) {
+        return res.status(400).send({
+            success: false,
+            message: 'startTime is after endTime!',
+        })
+    }
+
+    //check startTime is not in the past
+    const currentDate = new Date()
+    if (startDateTimeAdd < currentDate) {
+        return res.status(400).send({
+            success: false,
+            message: 'startTime must be in future!',
+        })
+    }
+
+    const _pitch = await Pitch.findOne({})
+    .where('_id').equals(req.body.pitch)
+    .populate({
+        path: 'pitchType',
+    })
+
+    const _price = await Price.find({})
+    .sort({time: 1})
+    .where('pitchType').equals(_pitch.pitchType)
+    .populate({
+        path: 'time'
+    })
+
+    //make flat startTime in _price
+    const alStartTime = _price.map(item => {
+        return item.time.startTime
+    })
+    //make flat endTime in _price
+    const alEndTime = _price.map(item => {
+        return item.time.endTime
+    })
+
+    //check startTime is in _price
+    // console.log(alStartTime)
+    // console.log(alEndTime)
+    // console.log(startTimeArray2Add)
+    // console.log(endTimeArray2Add)
+    if (!alStartTime.includes(startTimeArrayAdd[1]) || !alStartTime.includes(endTimeArrayAdd[1]) && !alEndTime.includes(startTimeArrayAdd[1]) || !alEndTime.includes(endTimeArrayAdd[1])) {
+        return res.status(400).send({
+            success: false,
+            message: 'this time don\'t have price!',
+        })
+    }
+
+    // console.log(alStartTime)
+    // console.log(alEndTime)
+
+    // check time not conflict with other bookingDetail
+    const bookingDetails = await BookingDetail.find({})
+    .where('pitch').equals(pitch)
+    .select('startTime endTime')
+    for (let i = 0; i < bookingDetails.length; i++) {
+        const startTimeArray = bookingDetails[i].startTime.split(' ')
+        const endTimeArray = bookingDetails[i].endTime.split(' ')
+        const startDate = startTimeArray[0].split('/')
+        const endDate = endTimeArray[0].split('/')
+        const startTimeArray2 = startTimeArray[1].split(':')
+        const endTimeArray2 = endTimeArray[1].split(':')
+        const startDateTime = new Date(startDate[2], startDate[1] - 1, startDate[0], startTimeArray2[0], startTimeArray2[1])
+        const endDateTime = new Date(endDate[2], endDate[1] - 1, endDate[0], endTimeArray2[0], endTimeArray2[1])
+
+        if (startDateTime.toString() === startDateTimeAdd.toString() || startDateTime.toString() == endDateTimeAdd.toString()) {
+            return res.status(400).send({
+                success: false,
+                code: '0000',
+                message: 'Booked already',
+            })
+        }
+
+        if (startDateTime < startDateTimeAdd && endDateTime > startDateTimeAdd) {
+
+            return res.status(400).send({
+                success: false,
+                code: '0101',
+                message: 'Booked already',
+            })
+        }
+        if (startDateTime < endDateTimeAdd && endDateTime > endDateTimeAdd) {
+            return res.status(400).send({
+                success: false,
+                code: '1010',
+                message: 'Booked already',
+            })
+        }
+        if (startDateTime > startDateTimeAdd && endDateTime < endDateTimeAdd) {
+            return res.status(400).send({
+                success: false,
+                code: '1001',
+                message: 'Booked already',
+            })
+        }
+        if (startDateTime < startDateTimeAdd && endDateTime > endDateTimeAdd) {
+            return res.status(400).send({
+                success: false,
+                code: '0110',
+                message: 'Booked already',
+            })
+        }
+
+    }
+
+    // create array Time of bookingDetail request
+    const startTimeArrayAdd2 = startTimeArrayAdd[1].split(':')
+    const startTimeArrayAdd3 = endTimeArrayAdd[1].split(':')
+    let le = 30
+    if(startTimeArrayAdd2[1] == '00')
+    le = 00
+    let arrTime = []
+    for (let i = startTimeArrayAdd2[0].toString(); i <= startTimeArrayAdd3[0]; i++) {
+        if(le == 00){
+            if(i<10){
+                if(i == startTimeArrayAdd2[0])
+                arrTime.push(i + ':' + '00')
+                arrTime.push('0' + i + ':' + '00')
+            }
+            else
+            arrTime.push(i + ':' + '00')
+            le = 30
+        }else{
+            if(i<10){
+                if(i == startTimeArrayAdd2[0])
+                arrTime.push(i + ':' + '30')
+                else
+                arrTime.push('0' + i + ':'+ '30')
+            }
+            else
+            arrTime.push(i + ':' + '30')
+            le = 00
+        }
+        if(i == startTimeArrayAdd3[0] && startTimeArrayAdd3[1] == '30'){
+            if(i<10)
+            arrTime.push('0' + i + ':'+ '30')
+            else
+            arrTime.push(i + ':' + '30')
+        }
+            
+    }
+
+    //compare time for money
+    let sumPrice = 0
+    for(let i = 0; i < _price.length; i++){
+        for(let j = 0; j < arrTime.length; j+=1){
+            if(arrTime[j] == _price[i].time.startTime && arrTime[j+1] == _price[i].time.endTime){
+                sumPrice += _price[i].price
+            }
+        }
+    }
+
+    console.log(sumPrice)
+    req.body.price = sumPrice
+
+    req.body.startDate = startTimeArrayAdd[0]
+    req.body.endDate = endTimeArrayAdd[0]
+
+    const status = await Status.findOne({status: 'ST1'})
+    req.body.status = status._id.toString()
+    
+    next()
 
 }
 
@@ -87,4 +520,7 @@ const validateResult = (req, res, next) => {
     next()
 }
 
-module.exports = { validatePost, validateDelete, validatePut, validateGetByID, validateResult, validatePostFunction }
+module.exports = { validatePost, validateDelete, validatePut
+    , validateGetByID, validateResult, validatePostFunction
+    , validateCheckout, validateCheckoutFunction
+    , validatePostConfirm, validatePostConfirmFunction }
--- server/routes/booking.routes.js
+++ server/routes/booking.routes.js
@@ -7,10 +7,83 @@ const {
     validatePut,
     validateDelete,
     validateResult,
-    validatePostFunction
+    validatePostFunction,
+    validateCheckout,
+    validateCheckoutFunction,
+    validatePostConfirmFunction,
+    validatePostConfirm
 } = require('../middlewares/booking')
 const Booking = require('../models/Booking')
+const BookingDetail = require('../models/BookingDetail')
 const User = require('../models/User')
+
+/**
+ * @POST /api/booking/checkout
+ * @desc check booking
+ */
+router.post('/checkout', verifyToken, validateCheckout(), validateResult, validateCheckoutFunction, async (req, res) => {
+    try {
+        const { startTime, endTime, customer, pitch } = req.body
+
+        res.status(200).json({
+            success: true,
+            message: 'success',
+            price: req.body.price,
+        })
+
+    }catch(err){
+        res.status(400).json({
+            success: false,
+            message: err.message
+        })
+    }
+})
+
+/**
+ * @POST /api/booking/confirm
+ * @desc confirm booking
+ */
+router.post('/confirm', verifyToken, validatePostConfirm(), validateResult, validatePostConfirmFunction, async (req, res) => {
+    try {
+        //create new booking
+        const { startDate, endDate, price, customer, pitch, startTime, endTime, status } = req.body
+        const booking = new Booking({
+            startDate,
+            endDate,
+            total: price,
+            ispaid: true,
+            customer,
+        })
+
+        const newBooking = await booking.save()
+
+        //create new bookingDetail
+        const bookingDetail = new BookingDetail({
+            booking: newBooking._id,
+            pitch,
+            startTime,
+            endTime,
+            status,
+            price,
+        })
+        const newBookingDetail = await bookingDetail.save()
+
+        res.status(200).json({
+            success: true,
+            message: 'success',
+            Booking: newBooking,
+            BookingDetail: newBookingDetail,
+        })
+
+
+    } catch (err) {
+        res.status(400).json({
+            success: false,
+            message: err.message
+        })
+    }
+})
+
 
 /**
  * @POST /api/booking
